22145355c2c0733d58dfd9474a53a99fe76f545e
Dhruv Parthasarathy <dhruv@udacity.com>
2015-03-01 22:18:46 -0800

    add initialise-db call and split into components

--- src/cljs/phonecat_re_frame/core.cljs
+++ src/cljs/phonecat_re_frame/core.cljs
@@ -4,20 +4,44 @@
               [secretary.core :as secretary :include-macros true]
               [goog.events :as events]
               [goog.history.EventType :as EventType]
-              [cljsjs.react :as react])
+              [cljsjs.react :as react]
+              [re-frame.core :as re-frame])
+    (:require-macros [reagent.ratom  :refer [reaction]])
     (:import goog.History))
+
+;; -------------------------
+;; Re-frame data
+
+(re-frame/register-subs        ;; a new subscription handler
+   :phones             ;; usage (subscribe [:phones])
+   (fn [db]
+     (reaction (:phones @db))))  ;; pulls out :phones
+
+(re-frame/register-pure-handler
+   :initialise-db             ;; usage: (dispatch [:initialise-db])
+   (fn 
+     [_ _]                   ;; Ignore both params (db and v). 
+     {:phones [{:name "Nexus S" :snippet "Fast just got faster with Nexus S."}
+               {:name "Motorola XOOM™ with Wi-Fi" :snippet "The Next, Next Generation tablet."}
+               {:name "Motoral Xoom" :snippet "The Next, Next Generation tablet."}]}))
 
 ;; -------------------------
 ;; Views
 
+(defn phone-component
+  [name snippet]
+  [:li
+   [:span name]
+   [:p snippet]])
+
+(defn phones-component
+  []
+  (let [phones (re-frame/subscribe [:phones])]
+    (fn []
+      [:ul (doall  (map (fn [phone] ^{:key phone} [phone-component (:name phone) (:snippet phone)]) @phones))])))
+
 (defn home-page []
-  [:ul
-   [:li
-    [:span "Nexus S"]
-    [:p "Fast just got faster with Nexus S."]]
-   [:li
-    [:span "Motorola XOOM with Wi-Fi"]
-    [:p "The Next, Next Generation tablet."]]])
+  [phones-component])
 
 (defn current-page []
   [:div [(session/get :current-page)]])
@@ -45,4 +69,5 @@
 ;; Initialize app
 (defn init! []
   (hook-browser-navigation!)
+  (re-frame/dispatch [:initialise-db])
   (reagent/render-component [current-page] (.getElementById js/document "app")))
