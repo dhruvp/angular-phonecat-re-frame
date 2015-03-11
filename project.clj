4354cc42db3d27f13a45d7300ad8e8665c7d9f46
Dhruv Parthasarathy <dhruv@udacity.com>
2015-03-10 20:14:32 -0700

    bump reframe up to 0.2.0

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                  [compojure "1.3.2"]
                  [selmer "0.8.0"]
                  [environ "1.0.0"]
-                 [re-frame "0.1.7"]]
+                 [re-frame "0.2.0"]]
 
   :plugins [
             [lein-cljsbuild "1.0.4"]
